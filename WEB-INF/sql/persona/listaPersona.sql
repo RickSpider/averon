5a0479200dbf96d1849468bb65a231f1ec545ba2
RickSpider <rrgi89@hotmail.com>
2025-11-25 21:32:26 -0300

    agregado left join

--- WEB-INF/sql/persona/listaPersona.sql
+++ WEB-INF/sql/persona/listaPersona.sql
@@ -7,6 +7,6 @@ p.email,
 p.telefono,
 p.fechanacimiento
 from personas p 
-join tipos td on td.tipoid = p.documentotipoid
+left join tipos td on td.tipoid = p.documentotipoid
 where p.empresaid = ?1
 order by p.personaid asc;
