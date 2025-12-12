cd29cfe5a379c3735df3d33f227d1af5f0861808
RickSpider <rrgi89@hotmail.com>
2025-12-12 11:17:56 -0300

    correccion

--- WEB-INF/sql/persona/buscarPersona.sql
+++ WEB-INF/sql/persona/buscarPersona.sql
@@ -1,10 +1,10 @@
 Select 
 p.personaid as id, 
 p.idInterno, 
-COALESCE(p.razonsocial, p.nombre || ' ' || p.apellido) AS nombre_completo, 
+COALESCE(NULLIF(p.razonsocial, ''), p.nombre || ' ' || p.apellido) AS nombre_completo, 
 COALESCE(td.tipo, p.documentonum, '')  as TipoDoc, 
 COALESCE(p.ruc, p.documentonum, '') AS documento
 from personas p 
 left join tipos td on td.tipoid = p.documentotipoid
-where p.empresaid = ?1
+where p.empresaid = 2
 order by p.personaid asc;
